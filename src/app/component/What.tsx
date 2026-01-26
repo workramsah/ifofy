7101568f4242252f43781621fee060a18fb13356
workramsah <workramsah@gmail.com>
2026-01-26 12:30:52 +0545

    fix error

--- src/app/component/What.tsx
+++ src/app/component/What.tsx
@@ -5,7 +5,7 @@ export default function What() {
         <>
             <div className="flex flex-col space-y-[24px]">
                 <h1 className="font-extrabold text-[36px] text-center">What Customers are Saying</h1>
-                <div className="flex space-x-[24px]   overflow-x-auto">
+                <div className="flex space-x-[24px]  overflow-x-auto scrollbar-hide">
                     <Anna /><Anna /><Anna /><Anna />
                 </div>
             </div>
